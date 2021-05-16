c674462355528edd941fd70670a643330cc29bb9
lucas <lucasmengue96@hotmail.com>
2021-05-16 15:09:17 -0300

    :package: calling utils

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,6 @@
 const Category = require("../models/Category");
 const Product = require("../models/Product");
+const { formatPrice } = require("../../lib/utils");
 
 module.exports = {
   create(req, res) {
