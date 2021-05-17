818fec59021b09c47068d77bde199ae703ecb5d6
lucas <lucasmengue96@hotmail.com>
2021-05-17 18:15:38 -0300

    :package: created PhotosUpload object

--- public/scripts.js
+++ public/scripts.js
@@ -30,3 +30,66 @@ const Mask = {
     }).format(value / 100); // 180,23 -> 18023 / 100 = 180.23
   },
 };
+
+const PhotosUpload = {
+  preview: document.querySelector("#photos-preview"),
+  uploadLimit: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+
+    Array.from(fileList).forEach((file) => {
+      const reader = new FileReader();
+
+      if (PhotosUpload.hasLimit(event)) return;
+
+      reader.onload = () => {
+        const image = new Image(); // <img>
+        image.src = String(reader.result);
+
+        const div = PhotosUpload.getContainer(image);
+
+        PhotosUpload.preview.appendChild(div);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload;
+    const { files: fileList } = event.target;
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`);
+      event.preventDefault();
+      return true;
+    }
+
+    return false;
+  },
+  getContainer(image) {
+    const div = document.createElement("div");
+    div.classList.add("photo");
+
+    div.onclick = PhotosUpload.removePhoto;
+
+    div.appendChild(image);
+    div.appendChild(PhotosUpload.getRemoveButton());
+
+    return div;
+  },
+  getRemoveButton() {
+    const button = document.createElement("i");
+
+    button.classList.add("material-icons");
+    button.innerHTML = "close";
+
+    return button;
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode;
+    const photosArray = Array.from(PhotosUpload.preview.children);
+    const index = photosArray.indexOf(photoDiv);
+
+    photoDiv.remove();
+  },
+};
