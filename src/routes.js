03660b2aa9305083e84984d5aba44af33226b42b
lucas <lucasmengue96@hotmail.com>
2021-05-24 15:19:56 -0300

    :package: calling HomeController.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const multer = require("./app/middlewares/multer");
 const ProductController = require("./app/controllers/ProductController");
+const HomeController = require("./app/controllers/HomeController");
 
 routes.get("/", function (req, res) {
   return res.render("layout.njk");
