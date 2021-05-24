44f037d5564aba045d2e96fc58246bda42498af2
lucas <lucasmengue96@hotmail.com>
2021-05-24 16:50:33 -0300

    :package: calling SearchController.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const multer = require("./app/middlewares/multer");
 const ProductController = require("./app/controllers/ProductController");
 const HomeController = require("./app/controllers/HomeController");
+const SearchController = require("./app/controllers/SearchController");
 
 // Home
 routes.get("/", HomeController.index);
