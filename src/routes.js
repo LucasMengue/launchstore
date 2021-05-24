25c86425b4dc9f4e19215c4b3071c88260642abd
lucas <lucasmengue96@hotmail.com>
2021-05-24 16:22:27 -0300

    :package: commenting on routes

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ const multer = require("./app/middlewares/multer");
 const ProductController = require("./app/controllers/ProductController");
 const HomeController = require("./app/controllers/HomeController");
 
+// Home
 routes.get("/", HomeController.index);
 
+// Products
 routes.get("/products/create", ProductController.create);
 routes.get("/products/:id", ProductController.show);
 routes.get("/products/:id/edit", ProductController.edit);
@@ -14,8 +16,10 @@ routes.post("/products", multer.array("photos", 6), ProductController.post);
 routes.put("/products", multer.array("photos", 6), ProductController.put);
 routes.delete("/products", ProductController.delete);
 
+// Alias
 routes.get("/ads/create", function (req, res) {
   return res.redirect("/products/create");
 });
 
+// Search
 module.exports = routes;
