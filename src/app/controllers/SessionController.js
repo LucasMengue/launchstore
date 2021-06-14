66735f96ebdc20d576aee5697ac44d08bec77f1e
lucas <lucasmengue96@hotmail.com>
2021-06-14 16:41:03 -0300

    :package: finishing the reset password function

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 
+const { hash } = require("bcryptjs");
 const crypto = require("crypto");
 const mailer = require("../../lib/mailer");
 
@@ -64,8 +65,9 @@ module.exports = {
   resetForm(req, res) {
     return res.render("session/password-reset", { token: req.query.token });
   },
-  reset(req, res) {
-    const { email, password, passwordRepeat, token } = req.body;
+  async reset(req, res) {
+    const user = req.user;
+    const { password, token } = req.body;
 
     try {
       // cria um no hash de senha
@@ -86,6 +88,8 @@ module.exports = {
     } catch (err) {
       console.error(err);
       return res.render("session/password-reset", {
+        user: req.body,
+        token,
         error: "Erro inesperado, tente novamente!",
       });
     }
