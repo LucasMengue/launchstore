dac4dd22fb8181f10e78fde4dc8ee040635371bd
lucas <lucasmengue96@hotmail.com>
2021-07-08 15:39:44 -0300

    :package: refactoring the search route on products

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,7 +8,7 @@ const SearchController = require("../app/controllers/SearchController");
 const { onlyUsers } = require("../app/middlewares/session");
 
 // Search
-routes.get("/products/search", SearchController.index);
+routes.get("/search", SearchController.index);
 
 // Products
 routes.get("/create", onlyUsers, ProductController.create);
